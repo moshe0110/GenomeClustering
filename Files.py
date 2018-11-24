9e5f3a82c1e026cf21c9d6fa236904784e3f06ca
moshe0110@gmail.com <moshe0110@gmail.com>
2018-11-24 10:52:13 +0200

    added

--- Files.py
+++ Files.py
@@ -1,4 +1,5 @@
 import os
 
 geo_all_100mb = os.path.join(os.path.dirname(__file__), 'GSE59114_C57BL6_GEO_all.xlsx')
-grover_5mb = os.path.join(os.path.dirname(__file__), 'GSE70657_Grover.A_et.al_RefSeq.Read.Count.csv')
+grover_5mb = os.path.join(os.path.dirname(__file__), 'GSE68981_FPKM_table_single-cell_RNA-seq_HSCspike.txt')
+gold_set = os.path.join(os.path.dirname(__file__), 'GSE64002_readcounts.csv')
--- main.py
+++ main.py
@@ -1,57 +1,55 @@
+import math
 import time
 
 from sklearn.cluster import AgglomerativeClustering, KMeans
-
+from Kcluster import FuzzyKMeans
 import Files
 import pandas as pd
-from sklearn import cluster,neighbors
-import networkx as nx
-from collections import defaultdict
-import matplotlib
+import scipy.stats.pearsonr
 
-#matplotlib.use('TkAgg')
-from matplotlib import cm
-import seaborn as sns
-import numpy as np
-from sklearn.metrics.cluster import normalized_mutual_info_score
-from sklearn.metrics.cluster import adjusted_rand_score
-
-import matplotlib.pyplot as plt
-
-def project1 ():
-    df = pd.read_csv(open(Files.grover_5mb, 'rb'))
-    df = df.fillna(0).to_sparse(fill_value=0)
-    # res = sklearn.cluster.k_means(df, 10)
-
-
-def print_clusters(X):
+def print_clusters(X,labels):
     for i in range(len(X)):
-        print("name:", X[i][0], "label:", labels[i])
-        #plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=10)
+        print("name:", X.iloc[[i]].index.values[0], "label:", labels[i])
+        # plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=10)
+
+
+def clean_null_figures(df: pd.DataFrame):
+    df = df[(df.T != 0).any()]
+    df = df.loc[:, (df != 0).any(axis=0)]
+    return df
+
+
+def log(x):
+    if x == 0:
+        return 0
+    else:
+        return math.log(x)
+
+
+def count_values_bigger_than_k(s: pd.Series, k: int):
+    count = 0
+    for i in s:
+        if i > k: count += 1
+    return count
+
+def clean_less_than_k(df: pd.DataFrame, value_threshold: int, rowcount_percentage:float):
+    row_count = df.shape[0]
+    row_threshold = rowcount_percentage*row_count
+    #mask = pd.Series([count_values_bigger_than_k(s, value_threshold)>row_threshold for s in df.values])
+    mask = pd.Series([count_values_bigger_than_k(s, value_threshold) > 10 for s in df.values],index=df.index)
+    return df[mask]
+
 
 if __name__ == '__main__':
-    # X = pd.DataFrame([[1, 2, 30],
-    #                   [5, 8, 3],
-    #                   [1.5, 1.8, 3],
-    #                   [8, 8, 30],
-    #                   [1, 0.6, 30],
-    #                   [9, 11, 3]])
-
-    X = pd.read_csv(open(Files.grover_5mb, 'rb'))
+    X: pd.DataFrame = pd.read_csv(open(Files.gold_set, 'rb'))
     X = X.set_index('Unnamed: 0')
-
-    #X = X.fill(0).to_sparse(fill_value=0)
-    kmeans = KMeans(n_clusters=5)
-    kmeans.fit(X)
-    X = X.values
-
-    centroids = kmeans.cluster_centers_
-    labels = kmeans.labels_
-
-
-    print_clusters(X)
-
-
-    plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=150, linewidths=5, zorder=10)
-
-    plt.show()
+    X = clean_null_figures(X)
+    X = X.applymap(log)
+    X = clean_less_than_k(X, 5,0.05)
+    # X = X.fill(0).to_sparse(fill_value=0)
+    AgglomerativeClustering(n_clusters=5,affinity=pearsonr).fit(X)
+    #kmeans = FuzzyKMeans(k=4,)
+    #kmeans.fit(X)
+    #centroids = kmeans.cluster_centers_
+    #labels = kmeans.labels_
+    print_clusters(X,labels)
